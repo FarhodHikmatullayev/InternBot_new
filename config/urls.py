753fb6f14925e5bcad0d7dc39a50d190938d2d5d
Farhodjon <farhodjonhikmatullayev@gmail.com>
2024-10-18 15:53:34 +0500

    modified

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('path-to-admin-site-secret8747/', admin.site.urls),
 ]
 
 if settings.DEBUG:
